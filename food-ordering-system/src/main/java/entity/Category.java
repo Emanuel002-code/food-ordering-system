a42fd48a10b7149607a8aa8020f0359bf8f9502c
Emanuel002-code <tumelomafalo77@gmail.com>
2026-06-12 09:28:00 +0200

    created category class

---
+++ food-ordering-system/src/main/java/entity/Category.java
@@ -0,0 +1,36 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Setter;
+
+
+@Setter //use lombok to  auto create set methods
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String name;
+
+
+    //default constructor of  category class
+    public Category(){}
+
+    //constructor or category class
+    public Category(Long id, String name)
+    {
+        this.name=name;
+        this.id=id;
+    }
+    // getter method for id
+    public Long getId(Long id)
+    {
+        return this.id;
+    }
+    //getter method for name
+    public String getName(String name)
+    {
+        return  this.name;
+    }
+}
