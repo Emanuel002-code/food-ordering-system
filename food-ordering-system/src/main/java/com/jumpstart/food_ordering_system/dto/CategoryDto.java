2899128848fd99397f2750fb6337b7873105e980
Emanuel002-code <tumelomafalo77@gmail.com>
2026-06-17 16:05:19 +0200

    feat(category): input validation

--- food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/dto/CategoryDto.java
+++ food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/dto/CategoryDto.java
@@ -5,11 +5,13 @@ package com.jumpstart.food_ordering_system.dto;
  */
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class CategoryDto
 {
     private Long id;
-    @NotBlank(message = "Category name cannot be empty")
+    @NotBlank(message = "Category name is required")
+    @Size(min=2, max=50, message = "Name must be 2-50 characters")
     private String name;
 
     //default constructor
