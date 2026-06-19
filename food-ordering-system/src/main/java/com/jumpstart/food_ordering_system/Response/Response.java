fcd63cd87870bcb36a14f62d8272a9c08bb78b76
Emanuel002-code <tumelomafalo77@gmail.com>
2026-06-19 15:57:52 +0200

    feat: static factory methods on Response

--- food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/Response/Response.java
+++ food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/Response/Response.java
@@ -1,7 +1,8 @@
 package com.jumpstart.food_ordering_system.Response;
 
 /*
-   This generic class wrap the responses of all endpoints
+ *This generic class wrap the responses of all endpoints
+ *This enables us to have a structured format for every response
  */
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
@@ -15,15 +16,32 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response<T> {
 
-    @Getter
-    @Setter
     private String message;
-    @Setter
-    @Getter
     private T data;
-    @Setter
-    @Getter
     private int statusCode;
-    @Getter
     private LocalDateTime timestamp;
+
+    /*
+       Generic method for a success response
+     */
+    public static <T>  Response<T> success(String message, T data)
+    {
+          return Response.<T>builder()
+                  .statusCode(200)
+                  .message(message)
+                  .data(data)
+                  .timestamp(LocalDateTime.now())
+                  .build();
+    }
+    /*
+     *Generic method for error response
+     */
+    public static  <T> Response<T>   error(int code, String message)
+    {
+        return  Response.<T>builder()
+                .statusCode(code)
+                .message(message)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
 }
