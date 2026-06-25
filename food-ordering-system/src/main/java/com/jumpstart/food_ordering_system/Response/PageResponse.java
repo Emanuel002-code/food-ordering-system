1e89bd7aa75103dbb554ea7330458553606be4b3
Emanuel002-code <tumelomafalo77@gmail.com>
2026-06-25 16:50:32 +0200

    PageResponse class create

---
+++ food-ordering-system/src/main/java/com/jumpstart/food_ordering_system/Response/PageResponse.java
@@ -0,0 +1,39 @@
+package com.jumpstart.food_ordering_system.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageResponse<T> {
+
+    private List<T> content;
+    private long totalElements;
+    private int totalPages;
+    private int number;
+    private int size;
+    private boolean first;
+    private boolean last;
+
+    public static  <T>  PageResponse<T> page(Page<T> page)
+    {
+        return  PageResponse.<T>builder()
+                .content(page.getContent())
+                .totalElements(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .number(page.getNumber())
+                .size(page.getSize())
+                .first(page.isFirst())
+                .last(page.isLast())
+                .build();
+    }
+
+
+}
